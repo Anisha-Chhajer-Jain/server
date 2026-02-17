b40c0849423e07e9032693339b05d570d8846f17
Anisha Chhajer <anishachhajer16@gmail.com>
2026-02-17 15:26:00 +0530

    task of server and http methods

--- index.js
+++ index.js
@@ -1,38 +1,145 @@
-// console.log("server ")
-// console.log("prashant is a dobaa")
-//4 and 6th line is an import
 const express = require("express");
-
 const app = express();
-//alt of mongodb
-const users = [
-  { id: 1, name: "Rishi", role: "student" },
-  { id: 2, name: "Samir Sir", role: "mentor" },
-  { id: 3, name: "Anisha", role: "student" },
-  { id: 4, name: "Prashant", role: "student" }
+
+app.use(express.json());
+
+let users = [
+    {
+        att: 95,
+        uid: 1,
+        total_sub: 14,
+        name: "Anisha",
+        bonus: 20
+    },
+    {
+        att: 78,
+        uid: 1,
+        total_sub: 14,
+        name: "Hetavi",
+        bonus: 17
+    },
+    {
+        att: 89,
+        uid: 3,
+        total_sub: 14,
+        name: "Prashant",
+        bonus: 19
+    },
+
+    {
+        att: 85,
+        uid: 4,
+        total_sub: 14,
+        name: "Shabdh",
+        bonus: 16
+    },
+    {
+        att: 84,
+        uid: 5,
+        total_sub: 14,
+        name: "Anshu",
+        bonus: 18
+    },
+    {
+        att: 80,
+        uid: 6,
+        total_sub: 14,
+        name: "Hanuman",
+        bonus: 12
+    }
 ];
-app.get("/", (req, res) => {   //route
-  res.send("Express server is running");
+
+app.get("/", (req, res) => {
+    // console.log("accepted request");
+    res.send("Express server is running");
 });
 
-app.get("/second", (req, res) => {   
-  res.send("Express server is running in second(2nd) route");
-});
 app.get("/users", (req, res) => {
-  res.status(200).json(users);
+    res.status(200).json(users);
 });
 
-app.get("/users/:id", (req, res) => {
-  const userId = Number(req.params.id);
-  const user = users.find(u => u.id === userId);
+app.get("/users/:uid", (req, res) => {
+    const userId = Number(req.params.uid);
+    const user = users.find(u => u.uid === userId);
 
-  if (!user) {
-    return res.status(404).json({ message: "User not found" });
-  }
-
-  res.status(200).json(user);
+    if (!user) {
+        return res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json(user);
 });
-//all routes should be written before app.listen
+
+// app.use(express.json());
+
+app.post("/users", (req, res) => {
+    const newUser = {
+        id: users.length + 1,
+        name: req.body.name,
+        att: req.body.att,
+        total_sub: req.body.total_sub
+    };
+
+    users.push(newUser);
+
+    res.status(201).json({
+        message: "User created",
+        user: newUser
+    });
+});
+
+app.put("/users/:uid", (req, res) => {
+    const userId = Number(req.params.uid);
+    const index = users.findIndex(u => u.uid === userId);
+
+    if (index === -1) {
+        return res.status(404).json({ message: "User not found" });
+    }
+
+    users[index] = {
+        uid: userId,
+        name: req.body.name,
+        att: req.body.att,
+        total_sub: req.body.total_sub,
+        bonus: req.body.bonus
+    };
+
+    res.status(200).json({
+        message: "User replaced successfully",
+        user: users[index]
+    });
+});
+
+
+app.patch("/users/:uid", (req, res) => {
+    console.log("req")
+    const userId = Number(req.params.uid);
+    const user = users.find(u => u.uid === userId);
+
+    if (!user) {
+        return res.status(404).json({ message: "User not found" });
+    }
+
+    if (req.body.name) user.name = req.body.name;
+    if (req.body.role) user.role = req.body.role;
+
+    res.status(200).json({
+        message: "User updated",
+        user
+    });
+});
+
+app.delete("/users/:uid", (req, res) => {
+    const userId = Number(req.params.uid);
+    const index = users.findIndex(u => u.uid === userId);
+
+    if (index === -1) {
+        return res.status(404).json({ message: "User not found" });
+    }
+
+    users.splice(index, 1);
+
+    res.status(204).end();
+});
+
 app.listen(3000, () => {
-  console.log("Server started on port 3000");
+    console.log("Server started on local 3000");
 });
---
+++ indexex.js
@@ -0,0 +1,40 @@
+// console.log("server ")
+// console.log("prashant is a dobaa")
+//4 and 6th line is an import
+const express = require("express");
+
+const app = express();
+//alt of mongodb
+const users = [
+  { id: 1, name: "Rishi", role: "student" },
+  { id: 2, name: "Samir Sir", role: "mentor" },
+  { id: 3, name: "Anisha", role: "student" },
+  { id: 4, name: "Prashant", role: "student" }
+];
+
+app.get("/", (req, res) => {   //route
+  res.send("Express server is running");
+});
+
+app.get("/second", (req, res) => {   
+  res.send("Express server is running in second(2nd) route");
+});
+
+app.get("/users", (req, res) => {
+  res.status(200).json(users);
+});
+
+app.get("/users/:id", (req, res) => {
+  const userId = Number(req.params.id);
+  const user = users.find(u => u.id === userId);
+
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+
+  res.status(200).json(user);
+});
+//all routes should be written before app.listen
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
